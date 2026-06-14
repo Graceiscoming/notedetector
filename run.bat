f3a5fe45e7319139d1720b1fc3d00e11d0ffdc85
Graceiscoming <203387695+Graceiscoming@users.noreply.github.com>
2026-06-14 14:45:19 +0700

    Create run.bat

---
+++ run.bat
@@ -0,0 +1,15 @@
+@echo off
+title AI Note Detector
+echo ==============================================
+echo       Starting AI Note Detector Web App
+echo ==============================================
+echo.
+echo Please wait while the server loads...
+echo.
+echo Once started, open your web browser and go to: 
+echo -^> http://127.0.0.1:8000
+echo.
+echo Press Ctrl+C to stop the server when you are done.
+echo.
+C:\Users\หลัก\AppData\Local\Programs\Python\Python310\python.exe -m uvicorn app:app
+pause
